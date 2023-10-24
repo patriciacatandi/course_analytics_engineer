36912d62810f1de90714299c341aa6cfd6d53750
patriciacatandi <patriciabcatandi@gmail.com>
2023-10-23 21:43:16 -0300

    adding modifications

--- projeto/models/gold/tb_mean_price_date.sql
+++ projeto/models/gold/tb_mean_price_date.sql
@@ -8,7 +8,7 @@
 
 SELECT
     "date",
-    ROUND(AVG(price)::numeric, 2) as mean_price,
+    ROUND(AVG(price)::numeric, 2) as mean_price, -- ?? ver qual a diferença desse price pro listings
     ROUND(AVG(adjusted_price)::numeric, 2) as mean_adjusted_price
 FROM silver_calendar
 GROUP BY "date"
